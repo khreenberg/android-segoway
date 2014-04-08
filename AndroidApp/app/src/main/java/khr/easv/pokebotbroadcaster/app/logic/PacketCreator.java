858ccc354a6c0f15dae8a5f946462b1f92e9962b
Antal János Monori <anthony@monori.me>
2014-04-08 14:04:38 +0200

    Added some extra documentation

--- AndroidApp/app/src/main/java/khr/easv/pokebotbroadcaster/app/logic/PacketCreator.java
+++ AndroidApp/app/src/main/java/khr/easv/pokebotbroadcaster/app/logic/PacketCreator.java
@@ -3,16 +3,16 @@ package khr.easv.pokebotbroadcaster.app.logic;
 public class PacketCreator {
 
     static final short
-            MOTOR_BIT_MASK         =  127, //TODO: Figure out what to do after updating the protocol.
-            MAX_MOTOR_POWER        =  100;
+            MOTOR_BIT_MASK         =  127, // 2^7-1
+            MAX_MOTOR_POWER        =  100; // See: http://www.lejos.org/nxt/nxj/api/lejos/robotics/DCMotor.html#setPower(int)
 
     static final short
-            LEFT_MOTOR_START_BIT   =    0,
-            RIGHT_MOTOR_START_BIT  =    8;
+            LEFT_MOTOR_START_BIT   =    0, // [|--------------]
+            RIGHT_MOTOR_START_BIT  =    8; // [-------|-------]
 
     static final short
-            POSITIVE_BIT           =    1,
-            NEGATIVE_BIT           =    0;
+            POSITIVE_BIT           =    1, // +
+            NEGATIVE_BIT           =    0; // -
 
     public static short createPacket(short leftMotorPower, short rightMotorPower) {
 
@@ -29,6 +29,7 @@ public class PacketCreator {
         short signBit = power < 0 ? NEGATIVE_BIT : POSITIVE_BIT;
         short absPower = (short)Math.abs(power);
 
+        // If the absolute value of the motor power is bigger than the constant, throw an exception
         if (absPower > MAX_MOTOR_POWER)
             throw new IllegalArgumentException("Absolute value of speed parameter must be less than " + MAX_MOTOR_POWER);
 
@@ -43,6 +44,7 @@ public class PacketCreator {
         short signBit = power < 0 ? NEGATIVE_BIT : POSITIVE_BIT;
         short absPower = (short)Math.abs(power);
 
+        // If the absolute value of the motor power is bigger than the constant, throw an exception
         if (absPower > MAX_MOTOR_POWER)
             throw new IllegalArgumentException("Absolute value of power parameter must be less than " + MAX_MOTOR_POWER);
 
