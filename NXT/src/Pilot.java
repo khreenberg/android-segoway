2f7d26d2b4f463007d18fe9e97a6b4b972aa8e2d
KHReenberg <khreenberg@gmail.com>
2014-04-08 13:59:03 +0200

    Cleans up some obsolete stuff

--- NXT/src/Pilot.java
+++ NXT/src/Pilot.java
@@ -5,17 +5,10 @@ import lejos.nxt.NXTMotor;
 
 public class Pilot {
 
-//	SegowayMotor leftMotor;
-//	SegowayMotor rightMotor;
     NXTMotor leftMotor, rightMotor;
 	Random rand;
 
 	public Pilot() {
-//		this.leftMotor = new SegowayMotor(MotorPort.A); // hardcoded
-//		this.rightMotor = new SegowayMotor(MotorPort.C); // hardcoded
-//		this.leftMotor.start();
-//		this.rightMotor.start();
-
 	    leftMotor = new NXTMotor( MotorPort.A );
 	    rightMotor = new NXTMotor( MotorPort.C );
 	}
@@ -29,12 +22,4 @@ public class Pilot {
 		         "\nRight: "+rightMotor.getPower()+":"+rightPower;
 		System.out.println(s);
 	}
-
-//	public void setLeftPower(int power) {
-//		this.leftMotor.setPower(power);
-//	}
-//
-//	public void setRightPower(int power) {
-//		this.rightMotor.setPower(power);
-//	}
 }
