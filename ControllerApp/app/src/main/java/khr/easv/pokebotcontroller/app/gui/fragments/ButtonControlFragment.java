bdd459e58ed91cdf0d25c6635ca72ed7fae255f3
KHReenberg <khreenberg@gmail.com>
2014-04-27 17:13:57 +0200

    Minor cleanup in ButtonControlFragment

--- ControllerApp/app/src/main/java/khr/easv/pokebotcontroller/app/gui/fragments/ButtonControlFragment.java
+++ ControllerApp/app/src/main/java/khr/easv/pokebotcontroller/app/gui/fragments/ButtonControlFragment.java
@@ -14,47 +14,47 @@ import khr.easv.pokebotcontroller.app.gui.views.ImageButtonIgnoreTransparency;
 
 public class ButtonControlFragment extends Fragment {
 
-    View root;
+    private View _root;
 
-    ImageButtonIgnoreTransparency
-        btnTurnLeft,    btnMoveForward, btnTurnRight,
-        btnRotateLeft,  btnCenter,      btnRotateRight,
-        btnBackLeft,    btnMoveBack,    btnBackRight;
+    private ImageButtonIgnoreTransparency
+            _btnTurnLeft,       _btnMoveForward,    _btnTurnRight,
+            _btnRotateLeft,     _btnCenter,         _btnRotateRight,
+            _btnBackLeft,       _btnMoveBack,       _btnBackRight;
 
-    HashSet<ImageButtonIgnoreTransparency> buttons;
+    private HashSet<ImageButtonIgnoreTransparency> _buttons;
 
-    // Required empty public constructor
+    /** Required empty public constructor */
     public ButtonControlFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.fragment_button_control, container, false);
+        _root = inflater.inflate(R.layout.fragment_button_control, container, false);
         initialize();
         // Inflate the layout for this fragment
-        return root;
+        return _root;
     }
 
-    void initialize(){
+    private void initialize(){
         initializeButtons();
         attachClickListener();
     }
 
-    void initializeButtons(){
-        buttons = new HashSet<ImageButtonIgnoreTransparency>(9);
-        buttons.add(btnTurnLeft     = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlTurnLeft));
-        buttons.add(btnMoveForward  = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlMoveForward));
-        buttons.add(btnTurnRight    = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlTurnRight));
-        buttons.add(btnRotateLeft   = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlRotateLeft));
-        buttons.add(btnCenter       = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlCenter));
-        buttons.add(btnRotateRight  = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlRotateRight));
-        buttons.add(btnBackLeft     = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlBackLeft));
-        buttons.add(btnMoveBack     = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlMoveBack));
-        buttons.add(btnBackRight    = (ImageButtonIgnoreTransparency) root.findViewById(R.id.btnControlBackRight));
+    private void initializeButtons(){
+        _buttons = new HashSet<ImageButtonIgnoreTransparency>(9);
+        _buttons.add(_btnTurnLeft = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlTurnLeft));
+        _buttons.add(_btnMoveForward = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlMoveForward));
+        _buttons.add(_btnTurnRight = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlTurnRight));
+        _buttons.add(_btnRotateLeft = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlRotateLeft));
+        _buttons.add(_btnCenter = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlCenter));
+        _buttons.add(_btnRotateRight = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlRotateRight));
+        _buttons.add(_btnBackLeft = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlBackLeft));
+        _buttons.add(_btnMoveBack = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlMoveBack));
+        _buttons.add(_btnBackRight = (ImageButtonIgnoreTransparency) _root.findViewById(R.id.btnControlBackRight));
     }
 
-    void attachClickListener(){
+    private void attachClickListener(){
         ControlClickListener listener = new ControlClickListener();
-        for( ImageButtonIgnoreTransparency b : buttons ) b.setOnClickListener(listener);
+        for( ImageButtonIgnoreTransparency b : _buttons) b.setOnClickListener(listener);
     }
 
     private class ControlClickListener implements View.OnClickListener{
@@ -62,23 +62,23 @@ public class ButtonControlFragment extends Fragment {
         public void onClick(View v) {
             if( v == null ) {
                 return; // Shouldn't happen, but it makes the following lines look prettier. :P
-            }else if(v == btnTurnLeft ) {
+            }else if(v == _btnTurnLeft) {
                 Logger.debug("btnTurnLeft clicked!");
-            }else if(v == btnMoveForward ) {
+            }else if(v == _btnMoveForward) {
                 Logger.debug("btnMoveForward clicked!");
-            }else if(v == btnTurnRight ) {
+            }else if(v == _btnTurnRight) {
                 Logger.debug("btnTurnRight clicked!");
-            }else if(v == btnRotateLeft ) {
+            }else if(v == _btnRotateLeft) {
                 Logger.debug("btnRotateLeft clicked!");
-            }else if(v == btnCenter ) {
+            }else if(v == _btnCenter) {
                 Logger.debug("btnCenter clicked!");
-            }else if(v == btnRotateRight ) {
+            }else if(v == _btnRotateRight) {
                 Logger.debug("btnRotateRight clicked!");
-            }else if(v == btnBackLeft ) {
+            }else if(v == _btnBackLeft) {
                 Logger.debug("btnBackLeft clicked!");
-            }else if(v == btnMoveBack ) {
+            }else if(v == _btnMoveBack) {
                 Logger.debug("btnMoveBack clicked!");
-            }else if(v == btnBackRight ){
+            }else if(v == _btnBackRight){
                 Logger.debug("btnBackRight clicked!");
             }
         }
