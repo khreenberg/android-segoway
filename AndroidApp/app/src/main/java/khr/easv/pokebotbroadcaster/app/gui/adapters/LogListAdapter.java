07dfdb64e8364d6c002c29a416fda03c8cb4ed29
KHReenberg <khreenberg@gmail.com>
2014-05-04 14:05:36 +0200

    Brain.LogListAdapter > Method extraction

--- AndroidApp/app/src/main/java/khr/easv/pokebotbroadcaster/app/gui/adapters/LogListAdapter.java
+++ AndroidApp/app/src/main/java/khr/easv/pokebotbroadcaster/app/gui/adapters/LogListAdapter.java
@@ -29,11 +29,7 @@ public class LogListAdapter extends ArrayAdapter<LogEntry>{
     @Override
     public View getView(int index, View view, ViewGroup parent) {
 
-        if (view == null) {
-            LayoutInflater li = (LayoutInflater) getContext().getSystemService(
-                    Context.LAYOUT_INFLATER_SERVICE);
-            view = li.inflate(R.layout.list_item_log_entry, null);
-        }
+        if (view == null) view = inflateView();
 
         LogEntry entry = _entries.get(index);
 
@@ -45,27 +41,30 @@ public class LogListAdapter extends ArrayAdapter<LogEntry>{
         return view;
     }
 
-    int getBackgroundColor(View view, int index, LogEntry entry){
+    private View inflateView() {
+        LayoutInflater li = (LayoutInflater) getContext().getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        return li.inflate(R.layout.list_item_log_entry, null);
+    }
+
+    private int getBackgroundColor(View view, int index, LogEntry entry){
+        int colorID = getBackgroundColorId(entry);
+        int color = view.getResources().getColor(colorID);
+        int alpha = index % 2 == 0 ? EVEN_ALPHA : ODD_ALPHA;
+        return Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color));
+    }
+
+    private int getBackgroundColorId(LogEntry entry) {
         int colorID;
         switch (entry.getTag()){
-            case DEBUG:
-                colorID = R.color.LOG_ENTRY_DEBUG;
-                break;
-            case INFO:
-                colorID = R.color.LOG_ENTRY_INFO;
-                break;
-            case ERROR:
-                colorID = R.color.LOG_ENTRY_ERROR;
-                break;
-            case WARNING:
-                colorID = R.color.LOG_ENTRY_WARNING;
-                break;
+            case DEBUG:     colorID = R.color.LOG_ENTRY_DEBUG;      break;
+            case INFO:      colorID  = R.color.LOG_ENTRY_INFO;      break;
+            case ERROR:     colorID = R.color.LOG_ENTRY_ERROR;      break;
+            case WARNING:   colorID = R.color.LOG_ENTRY_WARNING;    break;
             default:
                 colorID = -1; // = Color.WHITE
                 break;
         }
-        int color = view.getResources().getColor(colorID);
-        int alpha = index % 2 == 0 ? EVEN_ALPHA : ODD_ALPHA;
-        return Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color));
+        return colorID;
     }
 }
