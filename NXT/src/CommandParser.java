bbcb63b8e5fd89362c4c1585f49ed6cb493e79bf
Antal János Monori <anthony@monori.me>
2014-05-04 23:26:33 +0200

    Formatted and documented NXT app.

--- NXT/src/CommandParser.java
+++ NXT/src/CommandParser.java
@@ -1,31 +1,62 @@
 package src;
 
+/**
+ * @author anthonymonori, khreenberg
+ * CommandParser static class that parses a 16b/2B short packet which contains 
+ * left and right motor speed with a sign information in it. The motor power 
+ * value is between -100 and 100 and is stored as an int, as performance is 
+ * only an issue through the bluetooth connection. It is built upon a bit 
+ * manipulation technique called bit-shifting.
+ * 
+ *  @see https://github.com/khreenberg/android-segoway/wiki/Protocol
+ *  @see http://docs.oracle.com/javase/tutorial/java/nutsandbolts/op3.html
+ */
 public class CommandParser{
 
-    static final int LEFT_MOTOR_START_BIT   = 	0,
-                     RIGHT_MOTOR_START_BIT  = 	8;
+    static final int LEFT_MOTOR_START_BIT   = 	0, // [|--------------]
+                     RIGHT_MOTOR_START_BIT  = 	8; // [-------|-------]
 
     static final int MOTOR_POWER_MASK 		= 127, // 1111111 (7 1's)
                      SIGN_MASK        		=   1; //       1
 
-    static final int POSITIVE_BIT 			= 	1,
-                     NEGATIVE_BIT 			= 	0;
+    static final int POSITIVE_BIT 			= 	1, // +
+                     NEGATIVE_BIT 			= 	0; // -
 
-    public static int leftMotor(int packet) {
+    /**
+     * Method to parse left motor power throught bit-shifting, by using some constant masks declared in the class.
+     * Read more about the protocol here: http://docs.oracle.com/javase/tutorial/java/nutsandbolts/op3.html
+     * 
+     * @param packet the short packet received from the controller (phone).
+     * @return int representing motor value.
+     */
+    public static int leftMotor(short packet) {
         int signBit = getValue(packet, LEFT_MOTOR_START_BIT, SIGN_MASK);
         int sign = signBit == NEGATIVE_BIT ? -1 : 1;
         int motorSpeed = getValue( packet, LEFT_MOTOR_START_BIT + 1, MOTOR_POWER_MASK );
-        return motorSpeed * sign;
+        return motorSpeed * sign; // apply the sign to the absolute value of the motor power.
     }
 
-    public static int rightMotor(int packet) {
+    /**
+     * Method to parse right motor power throught bit-shifting, by using some constant masks declared in the class.
+     * Read more about the protocol here: http://docs.oracle.com/javase/tutorial/java/nutsandbolts/op3.html
+     * 
+     * @param packet the short packet received from the controller (phone).
+     * @return int representing the motor value
+     */
+    public static int rightMotor(short packet) {
         int signBit = getValue(packet, RIGHT_MOTOR_START_BIT, SIGN_MASK);
-        int sign = signBit == NEGATIVE_BIT ? -1 : 1;
+        int sign = signBit == NEGATIVE_BIT ? -1 : 1; // get value by comparing the mask to the value in the packet.
         int motorSpeed = getValue( packet, RIGHT_MOTOR_START_BIT + 1, MOTOR_POWER_MASK );
-        return motorSpeed * sign;
+        return motorSpeed * sign; // apply the sign to the absolute value of the motor power.
     }
 
-    private static int getValue(int packet, int offset, int mask) {
+    /**
+     * @param packet the packet to parse.
+     * @param offset the offset number where to start from.
+     * @param mask mask to apply.
+     * @return int parsed value from packet.
+     */
+    private static int getValue(short packet, int offset, int mask) {
         return mask & packet >> offset;
     }
 }
--- NXT/src/Receiver.java
+++ NXT/src/Receiver.java
@@ -3,16 +3,29 @@ package src;
 import java.io.DataInputStream;
 import java.io.IOException;
 
+/**
+ * @author anthonymonori, khreenberg
+ * Class for receiving and handling the input coming from an external bluetooth connection through a DataInputStream.
+ * Runs in it's own thread to ensure that it's not blocking anything else.
+ */
 public class Receiver extends Thread {
 
-	private DataInputStream input;
-	private Pilot pilot;
+	private DataInputStream input; 
+	private Pilot pilot; 
 
+	/**
+	 * Constructor for this Receiver object.
+	 * @param input the input stream for the bluetooth connection.
+	 * @param pilot the pilot that controls the motors.
+	 */
 	public Receiver(DataInputStream input, Pilot pilot) {
-		this.setInput(input);
-		this.pilot = pilot;
+		this.setInput(input); // set the input using the setter.
+		this.pilot = pilot; 
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#run()
+	 */
 	@Override
 	public void run() {
 		// Handle the input until the thread stops
@@ -22,16 +35,23 @@ public class Receiver extends Thread {
 	}
 
 
+    /**
+     * Method for handling the actual input from the data stream channel.
+     */
     void handleInput(){
         try{
-            int packet = input.readInt();
-            pilot.update(packet);
+            short packet = input.readShort(); // reading a short packet which is sent from the controller (phone).
+            pilot.update(packet); // update the pilot with the newly arrived packet from the controller (phone).
         }
         catch( IOException e ){
             System.out.println("Error while reading from stream.\n"+e.getLocalizedMessage());
         }
     }
 
+    /**
+     * Setter for the input global var.
+     * @param input DataInputStream to set the property
+     */
     private void setInput(DataInputStream input) {
     	this.input = input;
     }
