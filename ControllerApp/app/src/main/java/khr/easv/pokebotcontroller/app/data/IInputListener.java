bdde2c03747e222543903d0f360999ae86c8bcd7
KHReenberg <khreenberg@gmail.com>
2014-05-05 10:45:37 +0200

    Controller.IInputListener > Adds documentation

--- ControllerApp/app/src/main/java/khr/easv/pokebotcontroller/app/data/IInputListener.java
+++ ControllerApp/app/src/main/java/khr/easv/pokebotcontroller/app/data/IInputListener.java
@@ -1,5 +1,6 @@
 package khr.easv.pokebotcontroller.app.data;
 
+/** Implement this interface to be notified on input changes */
 public interface IInputListener {
     void onInput(float x, float y);
 }
